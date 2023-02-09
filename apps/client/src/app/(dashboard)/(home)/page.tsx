6a96a77e56b2f279be621b962f5c777be6f165eb
Alfie Jones <alfie.jones@squaredup.com>
2023-02-09 11:28:54 +0000

    added repo link

--- apps/client/src/app/(dashboard)/(home)/page.tsx
+++ apps/client/src/app/(dashboard)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
 import { authOptions } from "@pixeleye/auth";
 import { Button, Container } from "@pixeleye/ui";
 import { getServerSession } from "next-auth";
@@ -16,17 +17,28 @@ export default async function IndexPage() {
           <Link href="/add">Add project</Link>
         </Button>
       </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-4 mt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {projects?.map((project) => (
           <div
             key={project.id}
-            className="relative flex flex-col items-center justify-center w-full h-48 p-6 overflow-hidden text-center bg-white border rounded-lg shadow-sm border-neutral-200 dark:bg-neutral-800 dark:border-neutral-700"
+            className="relative flex flex-col items-center justify-center w-full h-48 p-6 text-center transition bg-white border border-gray-200 rounded-lg shadow-sm shadow group/card hover:shadow-md dark:hover:border-white hover:border-black dark:bg-gray-900 dark:border-gray-700"
           >
             <h3 className="text-lg">{project.name}</h3>
-            <p className="text-sm text-neutral-500 dark:text-neutral-400">
+            <p className="text-sm text-gray-500 dark:text-gray-400">
               {project.url}
             </p>
-            <Link href={`/project/${project.id}`}>Link</Link>
+            <Link href={`/project/${project.id}`} className="absolute inset-0">
+              <span className="sr-only">Project page</span>
+            </Link>
+            {project.url && (
+              <a
+                href={project.url}
+                className="absolute top-0 right-0 p-2 transition transform group/link bg-black border border-black rounded-full opacity-0 dark:hover:!bg-gray-900 hover:!bg-white dark:border-white group-hover/card:opacity-100 dark:bg-white group-hover/card:translate-x-3 group-hover/card:-translate-y-3"
+              >
+                <span className="sr-only">Repo url</span>
+                <ArrowTopRightOnSquareIcon className="w-5 h-5 text-white dark:text-black dark:group-hover/link:text-white group-hover/link:text-black" />
+              </a>
+            )}
           </div>
         ))}
       </div>
