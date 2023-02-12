33f881a80c91c523215c6700aed17ed18e967a12
Alfie Jones <alfie.jones@squaredup.com>
2023-02-12 23:04:48 +0000

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ const config = {
           "./tsconfig.json",
           "./apps/*/tsconfig.json",
           "./packages/*/tsconfig.json",
+          "./docs/tsconfig.json",
         ],
       },
       rules: {
