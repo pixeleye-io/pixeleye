44098989cb2f223642432335947399c25faa8e86
Alfie Jones <alfie.jones@hotmail.co.uk>
2023-08-06 22:05:50 +0100

    Update account.ts

--- apps/api-tests/src/setup/account.ts
+++ apps/api-tests/src/setup/account.ts
@@ -1,6 +1,6 @@
 import { writeFile } from "fs/promises";
 import { createAllSessions, deleteUsers } from "./credentialsSetup";
-import waitOn from "wait-on";
+import { fetch } from "undici";
 import * as url from "url";
 
 // TODO - ping the API to check if it's up before running tests
@@ -15,10 +15,33 @@ const setup = async () => {
     JSON.stringify(tokens)
   );
 
-  await waitOn({
-    resources: ["http://localhost:5000/v1/ping"],
-    timeout: 200_000,
-  });
+  let counter = 0;
+  let isUp = false;
+
+  for (let i = 0; i < 100; i++) {
+    await fetch("http://127.0.0.1:5000/v1/ping", {
+      method: "GET",
+    })
+      .then(async (res) => {
+        console.log(res)
+        if (res.status === 200) {
+          isUp = true;
+          return;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    if (isUp) {
+      break;
+    }
+
+
+    console.log("Waiting for API to be up...")
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    counter++;
+  }
 
   return async () => {
     await deleteUsers(tokens);
