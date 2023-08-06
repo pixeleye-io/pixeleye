583f9a48287beb65b6abe03906556b83facacfb0
Alfie Jones <alfie.jones@hotmail.co.uk>
2023-08-06 21:51:26 +0100

    adding wait-on

--- apps/api-tests/src/setup/account.ts
+++ apps/api-tests/src/setup/account.ts
@@ -1,5 +1,6 @@
 import { writeFile } from "fs/promises";
 import { createAllSessions, deleteUsers } from "./credentialsSetup";
+import waitOn from "wait-on";
 import * as url from "url";
 
 // TODO - ping the API to check if it's up before running tests
@@ -13,6 +14,11 @@ const setup = async () => {
     __dirname + "/../fixtures/sessions.json",
     JSON.stringify(tokens)
   );
+
+  await waitOn({
+    resources: ["http://localhost:5000/v1/ping"],
+    timeout: 200_000,
+  });
 
   return async () => {
     await deleteUsers(tokens);
