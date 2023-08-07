df0267b00fb07a6f3ec76396fe0f7dab30cebf8f
Alfie Jones <alfie.jones@hotmail.co.uk>
2023-08-07 22:57:53 +0100

    adding favicon

--- apps/marketing/src/app/layout.tsx
+++ apps/marketing/src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "./providers";
 import { cx } from "class-variance-authority";
-import Footer from "./footer"
+import Footer from "./footer";
 import Header from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,17 +21,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </head>
       <body
         className={cx(
           inter.className,
           "selection:bg-teal-950 selection:text-teal-50"
         )}
       >
-          <Providers>
-            <Header />
-            <div className="pt-16">{children}</div>
-            <Footer />
-          </Providers>
+        <Providers>
+          <Header />
+          <div className="pt-16">{children}</div>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
