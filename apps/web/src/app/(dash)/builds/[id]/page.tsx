aa4c8a052fd4f1349484b9142eedc4d3d05fbac6
Alfie Jones <alfie.jones@hotmail.co.uk>
2023-08-17 00:13:56 +0100

    Cleaning up

--- apps/web/src/app/(dash)/builds/[id]/page.tsx
+++ apps/web/src/app/(dash)/builds/[id]/page.tsx
@@ -1,43 +1,6 @@
 import { API } from "@/libs";
-import { Template } from "@/ui/template";
 import { Reviewer } from "@pixeleye/reviewer";
-import {
-  Table,
-  TableHeader,
-  TableRow,
-  TableHead,
-  TableBody,
-  TableCell,
-  Button,
-} from "@pixeleye/ui";
 import { cookies } from "next/headers";
-import Link from "next/link";
-import { notFound } from "next/navigation";
-
-const dummyBuild = {
-  id: "1",
-  branch: "main",
-  buildNumber: 11,
-  projectID: "1",
-  status: "unreviewed",
-  createdAt: new Date().toUTCString(),
-  updatedAt: new Date().toUTCString(),
-  errors: [],
-  sha: "123",
-  title: "Playground build",
-};
-
-const dummySnapshots = [
-  {
-    id: "1",
-    buildID: "1",
-    createdAt: new Date().toUTCString(),
-    name: "Button",
-    updatedAt: new Date().toUTCString(),
-    status: "unreviewed",
-    snapID: "1",
-  },
-];
 
 export default async function ProjectOverviewPage({
   params,
@@ -48,28 +11,24 @@ export default async function ProjectOverviewPage({
 }) {
   const buildID = params.id;
 
-
-  const [build, snapshots] = await Promise.all([API.get("/builds/{id}", {
-    params: {
-      id: buildID,
-    },
-    headers: {
-      cookie: cookies().toString(),
-    },
-  }),
-  await API.get("/builds/{id}/snapshots", {
-    params: {
-      id: buildID,
-    },
-    headers: {
-      cookie: cookies().toString(),
-    },
-  })
+  const [build, snapshots] = await Promise.all([
+    API.get("/builds/{id}", {
+      params: {
+        id: buildID,
+      },
+      headers: {
+        cookie: cookies().toString(),
+      },
+    }),
+    await API.get("/builds/{id}/snapshots", {
+      params: {
+        id: buildID,
+      },
+      headers: {
+        cookie: cookies().toString(),
+      },
+    }),
   ]);
 
-
-
-  return (
-      <Reviewer build={build} snapshots={snapshots} />
-  );
+  return <Reviewer build={build} snapshots={snapshots} />;
 }
