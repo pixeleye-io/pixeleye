ac3918a3b19123ab29d068323836be02102efe39
Alfie Jones <alfie.jones@hotmail.co.uk>
2023-09-25 23:53:36 +0100

    expanding grouping tests

--- apps/backend/app/processors/ingest_processor_test.go
+++ apps/backend/app/processors/ingest_processor_test.go
@@ -2,7 +2,7 @@ package processors
 
 import (
 	"bytes"
-	"reflect"
+	"slices"
 	"testing"
 
 	"github.com/pixeleye-io/pixeleye/app/models"
@@ -163,7 +163,6 @@ func TestGenerateBytesHash(t *testing.T) {
 	}
 }
 
-// TODO - expand these tests
 func TestGroupSnapshots(t *testing.T) {
 	testCases := []struct {
 		name       string
@@ -188,12 +187,12 @@ func TestGroupSnapshots(t *testing.T) {
 		{
 			name: "no baselines",
 			snapshots: []models.Snapshot{
-				{Name: "snap1", Variant: "variant1"},
-				{Name: "snap2", Variant: "variant1"},
-				{Name: "snap3", Variant: "variant1"},
+				{ID: "1", Name: "snap1", Variant: "variant1"},
+				{ID: "2", Name: "snap2", Variant: "variant1"},
+				{ID: "3", Name: "snap3", Variant: "variant1"},
 			},
 			baselines:  []models.Snapshot{},
-			new:        []string{"snap1", "snap2", "snap3"},
+			new:        []string{"1", "2", "3"},
 			unchanged:  [][2]models.Snapshot{},
 			unreviewed: [][2]models.Snapshot{},
 			changed:    [][2]models.Snapshot{},
@@ -203,9 +202,9 @@ func TestGroupSnapshots(t *testing.T) {
 			name:      "no snapshots",
 			snapshots: []models.Snapshot{},
 			baselines: []models.Snapshot{
-				{Name: "snap1", Variant: "variant1"},
-				{Name: "snap2", Variant: "variant1"},
-				{Name: "snap3", Variant: "variant1"},
+				{ID: "1", Name: "snap1", Variant: "variant1"},
+				{ID: "2", Name: "snap2", Variant: "variant1"},
+				{ID: "3", Name: "snap3", Variant: "variant1"},
 			},
 			new:        []string{},
 			unchanged:  [][2]models.Snapshot{},
@@ -216,59 +215,103 @@ func TestGroupSnapshots(t *testing.T) {
 		{
 			name: "no changes",
 			snapshots: []models.Snapshot{
-				{Name: "snap1", Variant: "variant1", SnapID: "snap1"},
-				{Name: "snap2", Variant: "variant1", SnapID: "snap2"},
-				{Name: "snap3", Variant: "variant1", SnapID: "snap3"},
+				{ID: "1", Name: "snap1", Variant: "variant1", SnapID: "snap1"},
+				{ID: "2", Name: "snap2", Variant: "variant1", SnapID: "snap2"},
+				{ID: "3", Name: "snap3", Variant: "variant1", SnapID: "snap3"},
 			},
 			baselines: []models.Snapshot{
-				{Name: "snap1", Variant: "variant1", SnapID: "snap1"},
-				{Name: "snap2", Variant: "variant1", SnapID: "snap2"},
-				{Name: "snap3", Variant: "variant1", SnapID: "snap3"},
+				{ID: "11", Name: "snap1", Variant: "variant1", SnapID: "snap1", Status: models.SNAPSHOT_STATUS_APPROVED},
+				{ID: "22", Name: "snap2", Variant: "variant1", SnapID: "snap2", Status: models.SNAPSHOT_STATUS_ORPHANED},
+				{ID: "33", Name: "snap3", Variant: "variant1", SnapID: "snap3", Status: models.SNAPSHOT_STATUS_UNCHANGED},
 			},
 			new:        []string{},
-			unchanged:  [][2]models.Snapshot{{{SnapID: "snap1"}, {SnapID: "snap1"}}, {{SnapID: "snap2"}, {SnapID: "snap2"}}, {{SnapID: "snap3"}, {SnapID: "snap3"}}},
+			unchanged:  [][2]models.Snapshot{{{ID: "1"}, {ID: "11"}}, {{ID: "2"}, {ID: "22"}}, {{ID: "3"}, {ID: "33"}}},
 			unreviewed: [][2]models.Snapshot{},
 			changed:    [][2]models.Snapshot{},
 			rejected:   [][2]models.Snapshot{},
 		},
 		{
-			name: "no changes, different order",
+			name: "unreviewed or changes",
 			snapshots: []models.Snapshot{
-				{Name: "snap1", Variant: "variant1", SnapID: "snap1"},
-				{Name: "snap2", Variant: "variant1", SnapID: "snap2"},
-				{Name: "snap3", Variant: "variant1", SnapID: "snap3"},
+				{ID: "1", Name: "snap1", Variant: "variant1", SnapID: "snap1"},
+				{ID: "2", Name: "snap1", Variant: "variant2", SnapID: "snap2"},
+				{ID: "3", Name: "snap3", Variant: "variant1", SnapID: "snap3"},
+				{ID: "4", Name: "snap4", Variant: "variant1", SnapID: "snap4"},
 			},
 			baselines: []models.Snapshot{
-				{Name: "snap3", Variant: "variant1", SnapID: "snap3"},
-				{Name: "snap1", Variant: "variant1", SnapID: "snap1"},
-				{Name: "snap2", Variant: "variant1", SnapID: "snap2"},
+				{ID: "11", Name: "snap1", Variant: "variant1", SnapID: "snap1", Status: models.SNAPSHOT_STATUS_UNREVIEWED},
+				{ID: "22", Name: "snap1", Variant: "variant2", SnapID: "snap4", Status: models.SNAPSHOT_STATUS_APPROVED},
+				{ID: "33", Name: "snap3", Variant: "variant1", SnapID: "snap5", Status: models.SNAPSHOT_STATUS_ORPHANED},
+				{ID: "44", Name: "snap4", Variant: "variant1", SnapID: "snap6", Status: models.SNAPSHOT_STATUS_REJECTED},
 			},
 			new:        []string{},
-			unchanged:  [][2]models.Snapshot{{{SnapID: "snap1"}, {SnapID: "snap1"}}, {{SnapID: "snap2"}, {SnapID: "snap2"}}, {{SnapID: "snap3"}, {SnapID: "snap3"}}},
-			unreviewed: [][2]models.Snapshot{},
-			changed:    [][2]models.Snapshot{},
+			unchanged:  [][2]models.Snapshot{},
+			unreviewed: [][2]models.Snapshot{{{ID: "1"}, {ID: "11"}}},
+			changed:    [][2]models.Snapshot{{{ID: "2"}, {ID: "22"}}, {{ID: "3"}, {ID: "33"}}, {{ID: "4"}, {ID: "44"}}},
 			rejected:   [][2]models.Snapshot{},
+		},
+		{
+			name: "A bit of everything",
+			snapshots: []models.Snapshot{
+				{ID: "1", Name: "snap1", Variant: "variant1", SnapID: "snap1"},
+				{ID: "2", Name: "snap1", Variant: "variant2", SnapID: "snap2"},
+				{ID: "3", Name: "snap3", Variant: "variant1", SnapID: "snap3"},
+				{ID: "4", Name: "snap4", Variant: "variant1", SnapID: "snap4"},
+				{ID: "5", Name: "snap5", Variant: "variant1", SnapID: "snap5"},
+				{ID: "6", Name: "snap6", Variant: "variant1", SnapID: "snap6"},
+				{ID: "7", Name: "snap7", Variant: "variant1", SnapID: "snap7"},
+				{ID: "8", Name: "snap8", Variant: "variant1", SnapID: "snap8"},
+				{ID: "9", Name: "snap9", Variant: "variant1", SnapID: "snap9"},
+			},
+			baselines: []models.Snapshot{
+				{ID: "11", Name: "snap1", Variant: "variant1", SnapID: "snap1", Status: models.SNAPSHOT_STATUS_UNREVIEWED},
+				{ID: "22", Name: "snap1", Variant: "variant2", SnapID: "snap4", Status: models.SNAPSHOT_STATUS_APPROVED},
+				{ID: "33", Name: "snap3", Variant: "variant1", SnapID: "snap3", Status: models.SNAPSHOT_STATUS_ORPHANED},
+				{ID: "44", Name: "snap4", Variant: "variant1", SnapID: "snap4", Status: models.SNAPSHOT_STATUS_REJECTED},
+				{ID: "88", Name: "snap8", Variant: "variant1", SnapID: "snap10", Status: models.SNAPSHOT_STATUS_ORPHANED},
+			},
+			new:        []string{"9", "5", "6", "7"},
+			unchanged:  [][2]models.Snapshot{{{ID: "3"}, {ID: "33"}}},
+			unreviewed: [][2]models.Snapshot{{{ID: "1"}, {ID: "11"}}},
+			changed:    [][2]models.Snapshot{{{ID: "2"}, {ID: "22"}}, {{ID: "8"}, {ID: "88"}}},
+			rejected:   [][2]models.Snapshot{{{ID: "4"}, {ID: "44"}}},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			actualNew, actualUnchanged, actualUnreviewed, actualChanged, actualRejected := groupSnapshots(tc.snapshots, tc.baselines)
-			if reflect.DeepEqual(tc.new, actualNew) {
-				t.Errorf("expected %v, but got %v", tc.new, actualNew)
+			for _, id := range actualNew {
+				if !slices.Contains(tc.new, id) {
+					t.Errorf("actual new expected %v, but got %v", tc.new, actualNew)
+				}
 			}
-			if reflect.DeepEqual(tc.unchanged, actualUnchanged) {
-				t.Errorf("expected %v, but got %v", tc.unchanged, actualUnchanged)
-			}
-			if reflect.DeepEqual(tc.unreviewed, actualUnreviewed) {
-				t.Errorf("expected %v, but got %v", tc.unreviewed, actualUnreviewed)
-			}
-			if reflect.DeepEqual(tc.changed, actualChanged) {
-				t.Errorf("expected %v, but got %v", tc.changed, actualChanged)
-			}
-			if reflect.DeepEqual(tc.rejected, actualRejected) {
-				t.Errorf("expected %v, but got %v", tc.rejected, actualRejected)
-			}
+
+			contains(t, tc.unchanged, actualUnchanged)
+			contains(t, tc.unreviewed, actualUnreviewed)
+			contains(t, tc.changed, actualChanged)
+			contains(t, tc.rejected, actualRejected)
+
 		})
+	}
+}
+
+func contains(t *testing.T, arr [][2]models.Snapshot, actual [][2]models.Snapshot) {
+	if len(arr) != len(actual) {
+		t.Errorf("expected %v, but got %v", arr, actual)
+	} else if len(arr) == 0 {
+		return
+	}
+	for _, a := range arr {
+		found := false
+		for _, b := range actual {
+			if a[0].ID == b[0].ID && a[1].ID == b[1].ID {
+				found = true
+			}
+		}
+
+		if !found {
+			t.Errorf("expected %v, but got %v", arr, actual)
+		}
 	}
 }
