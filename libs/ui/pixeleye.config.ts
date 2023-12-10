fabce91542ccb6b037153b996aa6bbf52bef9f44
Alfie Jones <alfie.jones@hotmail.co.uk>
2023-12-10 02:32:38 +0000

    Update pixeleye.config.ts

--- libs/ui/pixeleye.config.ts
+++ libs/ui/pixeleye.config.ts
@@ -1,6 +1,4 @@
-import { Config } from "pixeleye";
-
-const config: Config = {
+const config = {
   token: "pxi__YPZtxOa4y0cWeccM3piETioC6HAYYYg9:675M5_0KKxhHYWKDYU0hA",
   endpoint: "http://localhost:5000",
   storybookOptions: {
