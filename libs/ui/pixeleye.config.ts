46826b487f79d9d3de8beaffb5324af1ef7b41bd
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-01-24 15:12:11 +0000

    lowering concurrency

--- libs/ui/pixeleye.config.ts
+++ libs/ui/pixeleye.config.ts
@@ -1,5 +1,5 @@
 const config = {
-  token: "pxi__NqH145Wcmm_mxNOS7bW2aFqHF-aYrk7A:WWHpFiw0ses19x_gY7UZu",
+  token: "pxi__iwF3At6tTdxZA_t9QEeUPd2ZtwPs2EZ5:StPXy0NRrlixP3D--o7EJ",
   endpoint: "http://localhost:5000",
   storybookOptions: {
     variants: [
