0d1eaa3ba5f73dc7826aa364a1fb59f9e6593ec3
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-01-26 17:12:40 +0000

    update build status when approving snapshots

---
+++ apps/web/src/app/(dash)/builds/[id]/segments.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { queries } from "@/queries";
+import { useQuery } from "@tanstack/react-query";
+import { RegisterSegment } from "../../breadcrumbStore";
+
+export function BuildSegments({ buildID, projectID }: { buildID: string, projectID: string }) {
+
+    const { data: build } = useQuery(queries.builds.detail(buildID));
+
+    const { data: project } = useQuery(queries.projects.detail(projectID));
+
+    return (
+        <RegisterSegment
+            order={1}
+            reference="builds"
+            segment={[
+                {
+                    name: project?.name || "",
+                    value: `/projects/${build?.projectID}`,
+                },
+                {
+                    name: `#${build?.buildNumber}`,
+                    value: `/builds/${buildID}`,
+                    status: build?.status,
+                },
+            ]}
+        />
+    )
+}
