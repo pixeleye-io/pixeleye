b3d08f8957d64bf0840e9384dbfcd1fac35509de
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-01-27 22:37:04 +0000

    adding chat bubbles

---
+++ libs/reviewer/src/comparisons/chatNode.tsx
@@ -0,0 +1,124 @@
+import { NodeProps } from "reactflow";
+import { Avatar, Popover, PopoverTrigger, PopoverContent, Button, ScrollArea } from "@pixeleye/ui";
+
+export interface ChatNodeData {
+    avatarURL?: string;
+    author?: string;
+}
+
+
+function NewComment({
+    avatarURL,
+    author
+}: {
+    avatarURL?: string;
+    author?: string
+}) {
+    return (
+
+        <form action="#" className="relative flex-auto">
+            <div className="overflow-hidden">
+                <label htmlFor="comment" className="sr-only">
+                    Add your comment
+                </label>
+                <textarea
+                    rows={3}
+                    name="comment"
+                    id="comment"
+                    className="block w-full resize-none p-1 border rounded-lg bg-transparent py-1.5 text-on-surface outline-none border-outline placeholder:text-on-surface-variant sm:text-sm sm:leading-6"
+                    placeholder="Add your comment..."
+                    defaultValue={''}
+                />
+            </div>
+
+            <div className="flex justify-end pt-2 pl-3 pr-2">
+                <Button
+                    type="submit"
+                    size="sm"
+                >
+                    Comment
+                </Button>
+            </div>
+        </form>
+    )
+}
+
+function Comments({
+    comments
+}: {
+    comments: {
+        content: string;
+        authorURL?: string;
+        author?: string;
+        createdAt: string;
+    }[]
+}) {
+
+
+    return (
+        <ul role="list">
+            {comments.map((comment, i) => (
+                <li key={i} className="flex">
+                    <div className="flex-shrink-0">
+                        <Avatar className="!w-6 !h-6">
+                            <Avatar.Image alt={`${comment.author || "unknown"}'s profile picture`} src={comment.authorURL ?? ""} />
+                            <Avatar.Fallback className="!text-xs bg-tertiary-container text-on-tertiary-container">
+                                {comment.author?.charAt(0).toUpperCase() || "U"}
+                            </Avatar.Fallback>
+                        </Avatar>
+                    </div>
+                    <div className="ml-3">
+                        <p className="text-sm font-medium text-on-surface">{comment.author}</p>
+                        <div className="mt-1 text-sm text-on-surface-variant">
+                            <p>{comment.content}</p>
+                        </div>
+                        <div className="mt-2 text-sm space-x-2">
+                            <span className="text-on-surface-variant">{comment.createdAt}</span>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export function ChatNode({ data: {
+    avatarURL,
+    author
+
+} }: NodeProps<ChatNodeData>) {
+
+    return (
+        <Popover>
+            <PopoverTrigger onClick={(e) => e.stopPropagation()}>
+                <div className="scale-90 origin-bottom-left transition hover:scale-100 rounded-[9999px_9999px_9999px_0px] h-8 w-8 border border-outline flex items-center justify-center bg-tertiary z-50 -translate-y-full">
+                    <Avatar className="!w-6 !h-6">
+                        <Avatar.Image alt={`${author || "unknown"}'s profile picture`} src={avatarURL ?? ""} />
+                        <Avatar.Fallback className="!text-xs bg-tertiary-container text-on-tertiary-container">
+                            {author?.charAt(0).toUpperCase() || "U"}
+                        </Avatar.Fallback>
+                    </Avatar>
+                </div>
+
+            </PopoverTrigger>
+            <PopoverContent className="!p-2" side="right">
+                <ScrollArea className="!max-h-[300px] pb-4">
+                    <Comments comments={[{
+                        content: "This is a comment",
+                        createdAt: "Today",
+                    }, {
+                        content: "This is a comment",
+                        createdAt: "Today",
+                    }, {
+                        content: "This is a comment",
+                        createdAt: "Today",
+                    }, {
+                        content: "This is a comment",
+                        createdAt: "Today",
+                    }]} />
+                </ScrollArea>
+                <NewComment avatarURL={avatarURL} author={author} />
+            </PopoverContent>
+        </Popover>
+    )
+}
--- libs/reviewer/src/comparisons/draggableImage.tsx
+++ libs/reviewer/src/comparisons/draggableImage.tsx
@@ -2,14 +2,14 @@ import 'reactflow/dist/style.css';
 
 import { StaticImageData } from "next/image";
 import {
-  forwardRef, useCallback, useContext, useEffect, useId, useImperativeHandle, useMemo, useState,
+  forwardRef, useCallback, useEffect, useImperativeHandle, useMemo, useState,
 } from "react";
-import ReactFlow, { NodeProps, OnNodesChange, applyNodeChanges, Node, useReactFlow, OnMove, Viewport } from 'reactflow';
-import NextImage from "next/image";
-import { cx } from "class-variance-authority";
-import { StoreContext, store } from "../store";
+import ReactFlow, { OnNodesChange, applyNodeChanges, Node, useReactFlow, OnMove, Viewport } from 'reactflow';
+import { store } from "../store";
 import { useStore } from "zustand";
 import Background from "./background";
+import { ImageNode } from './imageNode';
+import { ChatNode } from './chatNode';
 
 
 interface ImageProps {
@@ -39,94 +39,6 @@ export type DraggableImageRef = {
   center: () => void;
 };
 
-interface ImageNodeData {
-  base: {
-    src: string | StaticImageData;
-    alt: string;
-    width: number;
-    height: number;
-  };
-  overlay?: {
-    src: string | StaticImageData;
-    alt: string;
-    width: number;
-    height: number;
-  };
-  secondBase?: {
-    src: string | StaticImageData;
-    alt: string;
-    width: number;
-    height: number;
-  };
-}
-
-function ImageNode({ data: {
-  base,
-  overlay,
-  secondBase
-} }: NodeProps<ImageNodeData>) {
-
-  const store = useContext(StoreContext)
-
-  const optimize = useStore(store, (state) => state.optimize);
-  const showOverlay = useStore(store, (state) => state.showDiff);
-  const singleSnapshot = useStore(store, (state) => state.singleSnapshot);
-
-
-  return (
-    <div className="active:cursor-grabbing">
-      <NextImage
-        key={`base - ${base.src.toString()}`}
-        quality={100}
-        priority
-        className={cx(
-          "pointer-events-none select-none",
-          singleSnapshot !== "head" && "hidden",
-          showOverlay && overlay && "brightness-[50%]"
-        )}
-        draggable={false}
-        alt={base.alt}
-        src={base.src}
-        unoptimized={!optimize}
-        placeholder={optimize ? "blur" : "empty"}
-      />
-      {secondBase && (
-        <NextImage
-          key={`second - base - ${secondBase.src.toString()}`}
-          quality={100}
-          priority
-          className={cx(
-            "pointer-events-none select-none",
-            singleSnapshot === "head" && "hidden",
-          )}
-          draggable={false}
-          alt={secondBase.alt}
-          src={secondBase.src}
-          placeholder={optimize ? "blur" : "empty"}
-          unoptimized={!optimize}
-        />
-      )}
-      {overlay && (
-        <NextImage
-          key={`overlay - ${overlay.src.toString()}`}
-          priority
-          quality={100}
-          className={cx(
-            (!showOverlay || singleSnapshot !== "head") && "opacity-0",
-            "pointer-events-none select-none absolute inset-0",
-          )}
-          draggable={false}
-          alt={overlay.alt}
-          src={overlay.src}
-          placeholder={optimize ? "blur" : "empty"}
-          unoptimized={!optimize}
-        />
-      )}
-    </div>
-
-  )
-}
-
 
 export const DraggableImage = forwardRef<DraggableImageRef, ImageProps>(
   function DraggableImage(
@@ -140,11 +52,15 @@ export const DraggableImage = forwardRef<DraggableImageRef, ImageProps>(
     ref
   ) {
 
-    const initialNodes = [
-      { id: '1', position: { x: 0, y: 0 }, width: 100, height: 100, data: { base, overlay, secondBase }, type: 'image' }];
+    const initialNodes: Node[] = [
+      { id: '1', position: { x: 0, y: 0 }, data: { base, overlay, secondBase }, type: 'image' },
+
+
+      { id: '2', position: { x: 0, y: 0 }, data: {}, type: 'chat' },
+    ];
 
     const [nodes, setNodes] = useState<Node[]>(initialNodes);
-    const nodeTypes = useMemo(() => ({ image: ImageNode }), []);
+    const nodeTypes = useMemo(() => ({ image: ImageNode, chat: ChatNode }), []);
 
     const singleSnapshot = useStore(store, (state) => state.singleSnapshot);
     const setSingleSnapshot = useStore(store,
@@ -178,21 +94,35 @@ export const DraggableImage = forwardRef<DraggableImageRef, ImageProps>(
 
     return (
       <div className="h-full w-full flex-col flex items-center bg-surface-container-low rounded border border-outline-variant">
-        <ReactFlow proOptions={{
-          hideAttribution: true
-        }} onPaneClick={onClick} minZoom={0.1} onWheelCapture={(e) => {
-          if (e.shiftKey) {
-            e.preventDefault();
-            e.stopPropagation();
-            const vp = getViewport();
+        <ReactFlow
+          proOptions={{
+            hideAttribution: true
+          }}
+          onPaneClick={onClick}
+          minZoom={0.1}
+          onWheelCapture={(e) => {
+            if (e.shiftKey) {
+              e.preventDefault();
+              e.stopPropagation();
+              const vp = getViewport();
 
-            setViewport({
-              x: vp.x - e.deltaX,
-              y: vp.y - e.deltaY,
-              zoom: vp.zoom,
-            })
-          }
-        }} onNodeClick={onClick} nodesFocusable={false} zoomOnDoubleClick={false} nodesDraggable={false} nodes={nodes} nodeTypes={nodeTypes} maxZoom={10} onNodesChange={onNodesChange} fitView onMove={onMove} >
+              setViewport({
+                x: vp.x - e.deltaX,
+                y: vp.y - e.deltaY,
+                zoom: vp.zoom,
+              })
+            }
+          }}
+          onNodeClick={onClick}
+          nodesFocusable={false}
+          zoomOnDoubleClick={false}
+          nodesDraggable={false}
+          nodes={nodes}
+          nodeTypes={nodeTypes}
+          maxZoom={10}
+          onNodesChange={onNodesChange}
+          fitView
+          onMove={onMove} >
           <Background />
         </ReactFlow>
       </div>
