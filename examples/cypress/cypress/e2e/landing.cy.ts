ae002c56c7d8c914b2452a107ada02c51a5870ef
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-01-29 22:34:35 +0000

    Adding cypress types

--- examples/cypress/cypress/e2e/landing.cy.ts
+++ examples/cypress/cypress/e2e/landing.cy.ts
@@ -1,8 +1,10 @@
 describe("Landing page - e2e", () => {
-
   it("Should take a screenshot of the landing page", () => {
     cy.visit("/");
     cy.get("kbd.ml-auto").should("be.visible");
+
+    cy.wait(1000);
+
     cy.pixeleyeSnapshot({ name: "landing" });
   });
 });
--- integrations/cypress/src/index.ts
+++ integrations/cypress/src/index.ts
@@ -1,4 +1,13 @@
 import { pixeleyeSnapshot } from "./snapshot";
+import { Options } from "./snapshot";
 export type { Options } from "./snapshot";
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      pixeleyeSnapshot(options: Options): Chainable<void>;
+    }
+  }
+}
 
 Cypress.Commands.add("pixeleyeSnapshot" as any, pixeleyeSnapshot as any);
