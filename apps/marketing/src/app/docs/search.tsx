6c37323806c6a69e43b0ffc91f1e7609fdd52568
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-02-01 15:58:46 +0000

    updating search to use next link

--- apps/marketing/src/app/docs/search.tsx
+++ apps/marketing/src/app/docs/search.tsx
@@ -2,11 +2,17 @@
 
 import { DocSearch as Search } from '@docsearch/react';
 import './search.css';
+import Link from 'next/link';
 
 
 export function DocSearch() {
 
     return (
-        <Search indexName='pixeleye' apiKey='b0c6d1ff6949bf05c049abe4209ad481' appId='DVHY2LO2GG' />
+        <Search
+            hitComponent={({ hit, children }) => <Link href={hit.url}>{children}</Link>}
+            indexName='pixeleye'
+            apiKey='b0c6d1ff6949bf05c049abe4209ad481'
+            appId='DVHY2LO2GG'
+        />
     )
 }
