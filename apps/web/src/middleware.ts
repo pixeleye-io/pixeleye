3bafc6413c6d8fb7ac99ab0e3c6c9dab2bc56a78
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-02-12 12:21:26 +0000

    Update middleware.ts

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -10,6 +10,7 @@ export async function middleware(request: NextRequest) {
     headers: {
       Cookie: request.cookies.toString(),
     },
+    credentials: "include",
   }).catch((err) => {
     return undefined;
   });
