70782c1ae8a9e6c88be48cbb5647bdfe1bd86f54
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-02-13 01:56:32 +0000

    Update page.tsx

--- apps/web/src/app/(dash)/(home)/settings/(user)/account/page.tsx
+++ apps/web/src/app/(dash)/(home)/settings/(user)/account/page.tsx
@@ -23,7 +23,7 @@ export default async function AccountPage({
     redirect(`/settings/org?team=${team.id}`);
   }
 
-  const cookie = decodeURI(cookies().toString());
+  const cookie = cookies().toString();
 
   const flow = await frontend
     .getSettingsFlow({
