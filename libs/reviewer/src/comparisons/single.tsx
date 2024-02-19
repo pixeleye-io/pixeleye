47d4c8c7a77435e627366d12cd41ef325d062e7a
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-02-19 15:31:50 +0000

    Adding button to switch between single view snapshots

--- libs/reviewer/src/comparisons/single.tsx
+++ libs/reviewer/src/comparisons/single.tsx
@@ -3,6 +3,8 @@ import { DraggableImage, DraggableImageRef } from "./draggableImage";
 import { useStore } from "zustand";
 import { StoreContext } from "../store";
 import { ReactFlowProvider } from "reactflow";
+import { ArrowPathIcon, ArrowsRightLeftIcon, Square2StackIcon } from "@heroicons/react/24/outline";
+import { Button } from "@pixeleye/ui";
 
 interface SingleProps {
   draggableImageRef?: RefObject<DraggableImageRef>;
@@ -13,6 +15,7 @@ export function Single({ draggableImageRef }: SingleProps) {
 
   const snapshot = useStore(store, (state) => state.currentSnapshot)!;
   const singleSnapshot = useStore(store, (state) => state.singleSnapshot)!;
+  const setSingleSnapshot = useStore(store, (state) => state.setSingleSnapshot)!;
 
   const validDiff = Boolean(
     snapshot.diffURL && snapshot.diffWidth && snapshot.diffHeight
@@ -26,10 +29,16 @@ export function Single({ draggableImageRef }: SingleProps) {
     <div className="overflow-hidden w-full h-full">
       <div className="flex h-full w-full overflow-hidden">
         <ReactFlowProvider>
-          <div className="flex flex-col h-full w-full justify-center items-center">
-            <p className="bg-surface-container-low border rounded-md border-outline px-2 py-1 text-sm mb-2">
-              {singleSnapshot === "head" ? "Changes" : "Baseline"}
-            </p>
+          <div className="flex flex-col h-full w-full justify-center items-center 4">
+            <div className="flex items-center justify-center mb-2 space-x-2">
+              <p className="bg-surface-container-low border rounded-md border-outline px-2 py-1 text-sm min-w-20 text-center">
+                {singleSnapshot === "head" ? "Changes" : "Baseline"}
+              </p>
+              <Button size="icon" onClick={() => setSingleSnapshot(singleSnapshot === "head" ? "baseline" : "head")} variant="ghost">
+                <ArrowsRightLeftIcon className="w-5 h-5 rotate-90" />
+              </Button>
+            </div>
+
             <DraggableImage
               ref={draggableImageRef}
               id={snapshot.id}
