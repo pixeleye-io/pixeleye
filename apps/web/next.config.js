378482b50d04bc4a143eca2054eadd7c80ffbf96
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-03-02 13:36:17 +0000

    cleaning up

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,22 +1,19 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@pixeleye/ui", "@pixeleye/api", "@pixeleye/tailwind"],
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
   output: process.env.PX_HOST === "docker" ? "standalone" : undefined,
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
   assetPrefix: process.env.NEXT_PUBLIC_PIXELEYE_HOSTING === "true" && process.env.NODE_ENV === "production" ? "https://app.pixeleye.io" : undefined,
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
   output: process.env.DOCKER_BUILD === "true" ? "standalone" : undefined,
   async rewrites() {
     return [
-      {
+      ...(process.env.BACKEND_URL ? [{
         source: "/api/:path*",
-        // eslint-disable-next-line turbo/no-undeclared-env-vars
         destination: process.env.BACKEND_URL + "/:path*",
-      }
+      }] : []),
     ];
   }
 };
