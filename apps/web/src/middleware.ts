9a957055668b15425203578a4924c61a00e34173
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-03-03 11:57:22 +0000

    Updating docker build

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -2,10 +2,30 @@ import { NextResponse } from "next/server";
 import { oryEndpoint } from "@pixeleye/auth";
 import type { NextRequest } from "next/server";
 import { Session } from "@ory/kratos-client";
+import { env } from "./env";
+
+// Bit of a hack to redirect self-hosted docker users to the correct backend URL.
+function handleSelfHostedRewrites(request: NextRequest) {
+  if (env.NEXT_PUBLIC_PIXELEYE_HOSTING === "true" || !env.BACKEND_URL) return;
+
+  const url = request.nextUrl.clone();
+  if (!url.pathname.startsWith("/api")) return;
+
+  const newURL = new URL(env.BACKEND_URL);
+
+  newURL.pathname = url.pathname.replace("/api", "");
+
+  return NextResponse.rewrite(newURL, {
+    request,
+  });
+}
 
 // Anyone not logged in is redirected to the login page.
 
 export async function middleware(request: NextRequest) {
+  const res = handleSelfHostedRewrites(request);
+  if (res) return res;
+
   const data = await fetch(oryEndpoint + "/sessions/whoami", {
     headers: {
       cookie: request.cookies
@@ -48,5 +68,6 @@ export const config = {
     "/settings/:path*",
     "/usage/:path*",
     "/invites/:path*",
+    "/api/:path*",
   ],
 };
