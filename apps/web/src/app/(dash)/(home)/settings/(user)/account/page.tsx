ce4171d604349b8283d209c3f684c3fd4e5ef70d
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-03-03 23:59:58 +0000

    adding method to verify email

--- apps/web/src/app/(dash)/(home)/settings/(user)/account/page.tsx
+++ apps/web/src/app/(dash)/(home)/settings/(user)/account/page.tsx
@@ -5,6 +5,8 @@ import { filterNodesByGroups } from "@ory/integrations/ui";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { SettingsTemplate } from "../../settingsTemplate";
+import { API } from "@/libs";
+import { Button } from "@pixeleye/ui";
 
 export default async function AccountPage({
   searchParams,
@@ -12,6 +14,11 @@ export default async function AccountPage({
   searchParams: Record<string, string>;
 }) {
   const team = await getTeam(searchParams);
+
+  const oryUser = await frontend.toSession({
+    cookie: cookies().toString(),
+  })
+
 
   // The flow is used to identify the settings and registration flow and
   // return data like the csrf_token and so on.
@@ -56,45 +63,67 @@ export default async function AccountPage({
     return <AuthNode fullButton={false} node={node} key={i} />;
   });
 
-  console.log(socialNodes);
+  const addressesToVerify = oryUser?.data?.identity?.verifiable_addresses?.filter(
+    ({ verified }) => !verified
+  );
+
+  const verifyUrl = getUrlForFlow("verification", new URLSearchParams({
+    return_to: `/settings/account?flow=${searchParams.flow}`,
+  }));
+
+
+
 
   return (
     <main>
       <ErrorsList messages={flow.ui.messages} />
-      {passwordNodes.length > 0 && (
-        <SettingsTemplate
-          title="Password"
-          description="Reset your password here"
-        >
-          <form
-            className="space-y-4"
-            action={flow.ui.action}
-            method={flow.ui.method}
+      {
+        addressesToVerify && addressesToVerify?.length > 0 && (
+          <Button asChild>
+            <a href={verifyUrl} className="my-4">
+              Verify email
+            </a>
+          </Button>
+        )
+      }
+      {
+        passwordNodes.length > 0 && (
+          <SettingsTemplate
+            title="Password"
+            description="Reset your password here"
           >
-            {defaultComponents}
-            {passwordNodes.map((node, i) => {
-              return <AuthNode fullButton={false} node={node} key={i} />;
-            })}
-          </form>
-        </SettingsTemplate>
-      )}
-      {socialNodes.length > 0 && (
-        <SettingsTemplate
-          title="Social Login"
-          description="Connect your social accounts here"
-        >
-          <form
-            className="space-y-4"
-            action={flow.ui.action}
-            method={flow.ui.method}
+            <form
+              className="space-y-4"
+              action={flow.ui.action}
+              method={flow.ui.method}
+            >
+              {defaultComponents}
+              {passwordNodes.map((node, i) => {
+                return <AuthNode fullButton={false} node={node} key={i} />;
+              })}
+            </form>
+          </SettingsTemplate>
+        )
+      }
+      {
+        socialNodes.length > 0 && (
+          <SettingsTemplate
+            title="Social Login"
+            description="Connect your social accounts here"
           >
-            {defaultComponents}
-            {socialNodes.map((node, i) => {
-              return <AuthNode fullButton={false} node={node} key={i} />;
-            })}
-          </form>
-        </SettingsTemplate>
-      )}
-    </main>
+            <form
+              className="space-y-4"
+              action={flow.ui.action}
+              method={flow.ui.method}
+            >
+              {defaultComponents}
+              {socialNodes.map((node, i) => {
+                return <AuthNode fullButton={false} node={node} key={i} />;
+              })}
+            </form>
+          </SettingsTemplate>
+        )
+      }
+    </main >
   );
 }
