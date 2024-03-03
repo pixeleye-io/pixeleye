54de7014e0a3b512746fea5d97d233523d2bf744
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-03-03 15:32:09 +0000

    Update bucket_presigner.go

--- apps/backend/platform/storage/bucket_presigner.go
+++ apps/backend/platform/storage/bucket_presigner.go
@@ -2,13 +2,41 @@ package storage
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+func replaceS3Endpoint(request *v4.PresignedHTTPRequest) {
+
+	if os.Getenv("PIXELEYE_HOSTING") == "true" {
+		return
+	}
+
+	s3Endpoint := os.Getenv("S3_ENDPOINT")
+	clientS3Endpoint := os.Getenv("CLIENT_S3_ENDPOINT")
+
+	if s3Endpoint == "" || clientS3Endpoint == "" {
+		return
+	}
+
+	httpURL := fmt.Sprint("http://%s", s3Endpoint)
+	httpsURL := fmt.Sprint("https://%s", s3Endpoint)
+
+	url := request.URL
+
+	if strings.HasPrefix(url, httpURL) {
+		request.URL = strings.Replace(url, httpURL, fmt.Sprint("http://%s", clientS3Endpoint), 1)
+	} else if strings.HasPrefix(url, httpsURL) {
+		request.URL = strings.Replace(url, httpsURL, fmt.Sprint("https://%s", clientS3Endpoint), 1)
+	}
+}
 
 // GetObject makes a presigned request that can be used to get an object from a bucket.
 // The presigned request is valid for the specified number of seconds.
@@ -24,6 +52,9 @@ func (presigner BucketClient) GetObject(ctx context.Context,
 		log.Printf("Couldn't get a presigned request to get %v:%v. Here's why: %v\n",
 			bucketName, objectKey, err)
 	}
+
+	replaceS3Endpoint(request)
+
 	return request, err
 }
 
@@ -42,5 +73,8 @@ func (presigner BucketClient) PutObject(ctx context.Context,
 		log.Printf("Couldn't get a presigned request to put %v:%v. Here's why: %v\n",
 			bucketName, objectKey, err)
 	}
+
+	replaceS3Endpoint(request)
+
 	return request, err
 }
