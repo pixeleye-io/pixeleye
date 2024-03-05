91ac3253fb3504cfbf87a191dec3d11d84b3842d
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-03-05 23:45:29 +0000

    Cleaning up

--- examples/cypress/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# @pixeleye/cypress-example
-
-## null
-
-### Patch Changes
-
-- Updated dependencies []:
-  - pixeleye@0.4.19
--- examples/cypress/pixeleye.config.ts
+++ examples/cypress/pixeleye.config.ts
@@ -1,4 +1,4 @@
 export default {
-  token: "pxi__9hI4JoYvGmyt3e7AYgmda6fY8LeJZaaL:Jw-gbMBbOd5RRbcOF-Q2-",
+  token: "your-token",
   endpoint: "http://localhost:5000",
 };
