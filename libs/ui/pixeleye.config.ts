b20b27e49b3f2a9efa8d9404786adea303f7b7aa
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-03-10 10:36:07 +0000

    Update pixeleye.config.ts

--- libs/ui/pixeleye.config.ts
+++ libs/ui/pixeleye.config.ts
@@ -1,9 +1,6 @@
-import { Config } from "pixeleye";
-
-const config: Config = {
+const config = {
   // eslint-disable-next-line turbo/no-undeclared-env-vars
   token: process.env.PIXELEYE_TOKEN!,
-
   storybookOptions: {
     variants: [
       {
