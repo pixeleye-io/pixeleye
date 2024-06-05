6c8b6a9482a97c28eaf66d570035496856623b0d
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-06-05 13:33:50 +0100

    Update astro.config.mjs

--- apps/marketing-astro/astro.config.mjs
+++ apps/marketing-astro/astro.config.mjs
@@ -5,5 +5,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercelServerless({
+    imageService: true,
+  })
 });
