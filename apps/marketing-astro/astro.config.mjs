b6330179e6a851c2016bbce3392011d334dd6afb
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-06-05 13:12:14 +0100

    adding vercel plugin

---
+++ apps/marketing-astro/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
+});
