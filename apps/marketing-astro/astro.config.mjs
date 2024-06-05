2bff13aec3854e67c78d217288725615c97092bc
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-06-05 13:44:17 +0100

    adding diff image

--- apps/marketing-astro/astro.config.mjs
+++ apps/marketing-astro/astro.config.mjs
@@ -3,11 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercelServerless({
+  adapter: vercel({
     imageService: true,
   })
 });
