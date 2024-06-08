2f2932fac26f8aa0a471b3c0ef7d93a145c9795d
Alfie Jones <alfie.jones@hotmail.co.uk>
2024-06-08 19:31:42 +0100

    Update astro.config.mjs

--- apps/marketing-astro/astro.config.mjs
+++ apps/marketing-astro/astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "static",
+  // output: "hybrid",
   adapter: vercel({
     imageService: true,
   })
